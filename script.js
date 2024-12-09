83e5779cd90cfa8d07f996a689f24be527ebbf64
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-12-09 14:28:33 +0400

    added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// Required Core Stylesheet
-@import "node_modules/@glidejs/glide/src/assets/sass/glide.core";
 
-// Optional Theme Stylesheet
-@import "node_modules/@glidejs/glide/src/assets/sass/glide.theme";
+  document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header'); // Target the header element
+
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 50) { // Adjust threshold as needed
+        header.classList.add('scrolled'); // Add the `scrolled` class
+      } else {
+        header.classList.remove('scrolled'); // Remove the `scrolled` class
+      }
+    });
+  });
+
